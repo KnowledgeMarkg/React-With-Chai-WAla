af1c183e6d4c006f017f20ba7047bb7ecdf96854
Kausar Raza Ansari <122892146+KnowledgeMarkg@users.noreply.github.com>
2023-09-20 19:58:38 +0530

    counterButton
    counterButton

---
+++ ReactAndTailwindCSS/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+    <h1>Kausar Raza Tailwind CSS</h1>
+    </>
+  )
+}
+
+export default App
